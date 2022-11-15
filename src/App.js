6a2d7161d6687f8ab91ac4e1082d7f69a165320f
APURVA-ARYA <aryaapurva728@gmail.com>
2022-11-15 20:18:29 +0530

    testing  card Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Card from './Components/Card'
 
 function App() {
   return (
     <div className="App">
-     <h1>hey</h1>
+     <h1>hey below is a card Component</h1>
+<Card/>
      
     </div>
   );
---
+++ src/Components/Card.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <div className="card">This is trial Card component of dashboard</div>
+  )
+}
+
+export default Card
