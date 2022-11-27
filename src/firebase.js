3bda1530a4eb79fe3454bf27d9a08e586ca646a0
APURVA-ARYA <aryaapurva728@gmail.com>
2022-11-27 21:39:45 +0530

    firestore added

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,8 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "@firebase/firestore"
+import { getAnalytics } from "firebase/analytics";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCqW73KiOHD2ERtwtqCStNBzh8MN4VNQnQ",
   authDomain: "ami-education-fb684.firebaseapp.com",
@@ -19,3 +16,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const firestore = getFirestore(app);
