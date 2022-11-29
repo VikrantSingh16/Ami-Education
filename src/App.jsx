987327b79f6459b65e8f8e576a197eba380440f4
APURVA-ARYA <aryaapurva728@gmail.com>
2022-11-29 21:07:30 +0530

    loginsignup added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import React from 'react'
 import './assets/libs/boxicons-2.1.1/css/boxicons.min.css'
 import './scss/App.scss'
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Blank from './pages/Blank'
 import Notes from './pages/Notes'
@@ -12,7 +14,37 @@ import { DsaCodingQ } from './pages/DsaCodingQ'
 import { RoadMap } from './pages/RoadMap'
 import {Notifications} from './pages/Notifications'
 import Syllabus from './pages/Syllabus'
+import Login from './pages/Login'
+import Home from './pages/Home'
+import Register from "./pages/Register";
+import { auth } from './firebase';
+import { onAuthStateChanged } from 'firebase/auth'
 function App() {
+
+    const [user, setUser] = React.useState(null);
+    const [authState, setAuthState] = React.useState(null)
+  
+    React.useEffect(() => {
+      const unSubscribeAuth = onAuthStateChanged(auth,
+        async authenticatedUser => {
+          if(authenticatedUser) {
+            setUser(authenticatedUser.email)
+            setAuthState('home');
+          } else {
+            setUser(null);
+            setAuthState('login')
+          }
+        })
+  
+        return unSubscribeAuth;
+    }, [user])
+
+    if(authState === null) return <div className='font-bold text-center text-5xl'>loading...</div>
+    if(authState === 'login') return <Login setAuthState={setAuthState} setUser={setUser}/>
+    if(authState === 'register') return <Register setAuthState={setAuthState} setUser={setUser}/> 
+    if(user) return <Home user={user} setAuthState={setAuthState} setUser={setUser}/>
+  
+
     return (
         <BrowserRouter>
             <Routes>
@@ -24,7 +56,7 @@ function App() {
                     <Route path="placement" element={<Placement/>} />
                     <Route path="youtuberes" element={<YouTubeRes/>} />
                     <Route path="dsares" element={<DsaCodingQ/>} />
-                    <Route path="roadmap" element={<RoadMap/>} />
+                    <Route path="roadmap" element={<Login/>} />
 <Route path="notifications" element={<Notifications/>}/>
                 </Route>
             </Routes>
