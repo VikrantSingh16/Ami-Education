6ed76f78b97dc771a531d69ec09cbc1e27a64045
APURVA-ARYA <aryaapurva728@gmail.com>
2022-11-30 10:22:16 +0530

    refined

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -50,7 +50,7 @@ function Login({
                 </div>
                 <div className='mt-8 flex justify-between items-center'>
                     <div>
-                        <input  type="checkbox" id='remember'/>
+                        {/* <input  type="checkbox" id='remember'/> */}
                         {/* <label className='ml-2 font-medium text-base' for="remember">Remember for 30 days</label> */}
                     </div>
                     {/* <button className='font-medium text-base text-violet-500'>Forgot password</button> */}
